4862f7de797913ca4a45ce33d3eb4bf7788e88cf
00DZnkbl00 <dawidzdrojewski12345678@gmail.com>
2023-12-18 20:06:56 +0100

    Project generated in spring initializr

---
+++ src/main/java/pl/dawidzjava/foodReservationApp/FoodReservationAppApplication.java
@@ -0,0 +1,13 @@
+package pl.dawidzjava.foodReservationApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodReservationAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodReservationAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/dawidzjava/foodReservationApp/FoodReservationAppApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.dawidzjava.foodReservationApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodReservationAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
